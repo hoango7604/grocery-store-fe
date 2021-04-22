d76dc737102d0a20f48e2a504b6354873e359885
Hoa Ngo <hoa.ngo@vexere.com>
2021-04-22 22:04:47 +0700

    init project using typescript and add eslint for formatting

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,14 @@
+import React from 'react'
 import { AppProps } from 'next/app'
 
 import GlobalStyle from '../styles/global.style'
 
-const App = ({ Component, pageProps } : AppProps) => {
-  return (
-    <>
-      <GlobalStyle />
+const App = ({ Component, pageProps } : AppProps) => (
+  <>
+    <GlobalStyle />
 
-      <Component { ...pageProps } />
-    </>
-  )
-}
+    <Component {...pageProps} />
+  </>
+)
 
 export default App
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,48 @@
 import React from 'react'
-import Head from 'next/head';
+import Head from 'next/head'
 import Link from 'next/link'
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Container = styled.div`
   max-width: 1200px;
   margin: 0 auto;
   overflow: hidden;
   text-align: center;
-`;
 
-const Home = () => {
-  return (
-    <>
-      <Head>
-        <title>SSR styled-components with Next.js Starter</title>
-      </Head>
+  .hidden-button {
+    padding: 0;
+    border: 0;
+    margin: 0;
+    background-color: unset;
+  }
 
-      <Container>
-        <h1>Hello, world!</h1>
+  .text-link {
+    color: blue;
+    font-size: 16px;
+    text-decoration: underline;
+  }
+`
 
-        <p>
-          Read{' '}
+const Home = () => (
+  <>
+    <Head>
+      <title>SSR styled-components with Next.js Starter</title>
+    </Head>
 
-          <Link href="/posts/first-post">
-            <a>this page!</a>
-          </Link>
-        </p>
-      </Container>
-    </>
-  );
-}
+    <Container>
+      <h1>Hello, world!</h1>
+
+      <p>
+        {'Read '}
+
+        <Link href="/posts/first-post">
+          <button type="button" className="hidden-button">
+            <span className="text-link">this page!</span>
+          </button>
+        </Link>
+      </p>
+    </Container>
+  </>
+)
 
 export default Home
